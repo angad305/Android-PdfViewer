9515a228792c894aab9563fd01922a02bedfa986
Bhuvaneshwaran <bhuvaneshwaran319@gmail.com>
2024-12-27 15:08:24 +0530

    Fix callback

--- app/src/main/java/com/acutecoder/pdfviewerdemo/ComposePdfViewerActivity.kt
+++ app/src/main/java/com/acutecoder/pdfviewerdemo/ComposePdfViewerActivity.kt
@@ -168,7 +168,9 @@ private fun Activity.MainScreen(
             PdfViewer(
                 modifier = Modifier.fillMaxSize(),
                 containerColor = MaterialTheme.colorScheme.surfaceContainer,
-                afterLoadSource = {
+                onReady = { loadSource ->
+                    loadSource()
+
                     pdfSettingsManager.restore(this)
                     setPdfViewer(this)
                     addListener(object : PdfListener {
@@ -412,7 +414,9 @@ private fun Activity.MainScreenWithScrollModeSupport() {
             PdfViewer(
                 modifier = Modifier.fillMaxSize(),
                 containerColor = Color.Transparent,
-                afterLoadSource = {
+                onReady = { loadSource ->
+                    loadSource()
+
                     showPageButtons = pageScrollMode == PdfViewer.PageScrollMode.SINGLE_PAGE
                     showPageNumber = pageScrollMode == PdfViewer.PageScrollMode.SINGLE_PAGE ||
                             pageScrollMode == PdfViewer.PageScrollMode.HORIZONTAL
