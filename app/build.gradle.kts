92d7301a511f440960eaa31b72c724086b3a8a8f
Bhuvaneshwaran <bhuvaneshwaran319@gmail.com>
2025-04-06 18:43:55 +0530

    Set version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bhuvaneshw.pdfviewerdemo"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 110
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
