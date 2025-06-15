f11051b2cb0e31f7f78c2fbd2670bfceaa1aef77
Bhuvaneshwaran <bhuvaneshwaran319@gmail.com>
2025-06-15 23:52:14 +0530

    Bump dependencies to latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 110
-        versionName = "1.1.1"
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
